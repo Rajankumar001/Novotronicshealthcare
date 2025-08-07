12db51a8b977e116e71f2b78d86129efb8b31ffc
Rajankumar001 <chaudharyrajan387@gmail.com>
2025-08-07 12:32:27 +0530

    changes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -22,7 +22,7 @@ function ContactForm() {
     setStatus('Sending...');
 
     try {
-      const res = await fetch('http://localhost:5000/send-email', {
+      const res = await fetch('https://novotronicshealthcareserver.onrender.com/send-email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
